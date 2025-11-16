45258556305afab96e3de148436af3f6a6a7a611
ngocqui183196 <qui.nguyen183196@gmail.com>
2025-11-16 14:55:29 +0700

    First build core

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  form: FormGroup;
   title = 'mbc-clone';
+
+   foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      inputDemo: ['', Validators.required],
+      selectDemo: ['']
+    })
+  }
 }
