339fc460b67090130b754e2f793916d31369d040
ngocqui183196 <qui.nguyen183196@gmail.com>
2026-01-05 14:10:04 +0700

    add cursor pointer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, HostListener, inject, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule,
+import {
+  MatSelectModule,
   //  MatSelectTrigger 
-  } from '@angular/material/select';
+} from '@angular/material/select';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
@@ -39,6 +40,50 @@ interface Account {
 })
 export class AppComponent {
 
+
+  @ViewChild('cursor', { static: true }) cursor!: ElementRef<HTMLDivElement>;
+
+
   constructor() {
+
+    this.updateCursorScale();
+
+    window.visualViewport?.addEventListener('resize', () => {
+      this.updateCursorScale();
+    });
+  }
+
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    const el = this.cursor.nativeElement;
+    el.style.left = e.clientX + 'px';
+    el.style.top = e.clientY + 'px';
+  }
+
+  @HostListener('document:mouseover', ['$event'])
+  onMouseOver(e: MouseEvent) {
+    const target = e.target as HTMLElement;
+
+    const isPointer =
+      target.closest('a, button, [role="button"]') ||
+      getComputedStyle(target).cursor === 'pointer';
+
+    if (isPointer) {
+      this.cursor.nativeElement.style.backgroundImage =
+        "url('/assets/cursor/img-pointer.svg')";
+    } else {
+      this.cursor.nativeElement.style.backgroundImage =
+        "url('/assets/cursor/img-default.svg')";
+    }
+  }
+
+  updateCursorScale() {
+    const scale = window.visualViewport?.scale || 1;
+
+    document.documentElement.style.setProperty(
+      '--cursor-scale',
+      scale.toString()
+    );
   }
 }
